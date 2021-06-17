b4fe630cc1f9fbe3618478c7931b4f1ab9279634
code-lover-jarvis <77064989+code-lover-jarvis@users.noreply.github.com>
2021-06-17 12:51:04 +0530

    Update Myscrappycode.py

--- Myscrappycode.py
+++ Myscrappycode.py
@@ -1,104 +1,187 @@
 from selenium import webdriver
 from time import sleep
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
+from datetime import datetime
 import re
+import csv
+import pickle
 from selenium.webdriver.common.keys import Keys
 import json
 import pandas as pd
 from glob import glob            #to find the filename of csv
+starttime = datetime.now()
 
-
-filename=glob(r"path to particular csv file/*.csv")[0]
+filename=glob(r"C:\Users\path to the csv file\*.csv")[0]
 
 options = Options()
-options.headless=True
+# options.headless=True
                                             #to hide the browser, to make it visible, comment the line
 
-usr='some user id'
-pwd='corresponding password'
 r=list()
-rr=list()
+btn=[1,2,3]
 retail=list()
 discount=list()
 skucode=list()
+fskucode=list()
+sk = dict()
+pgnum = 1
 
-# options.add_experimental_option("excludeSwitches", ["enable-logging"])           #if you have error related to the 'bluetooth adapter failed' which is caused due to version mismatch between the webdriver and chrome
-driver = webdriver.Chrome(options=options, executable_path=r'''C:\Users\username\.wdm\drivers\chromedriver\win32\85.0.4183.87\chromedriver.exe''')
-driver.get('the url to website that requires login')
-print ("Website Opened")
-sleep(3)
+baseurl="https://***.***/***/****/all-products/resale"
 
-driver.find_element_by_name('emailOrMobile').send_keys(usr)        #entering id
-print ("Id entered")
-
-driver.find_element_by_name('loginPassword').send_keys(pwd)          #entering password
-print ("Password entered")
-
-driver.find_element_by_xpath('''//*[@id="root"]/div/div[1]/div[2]/div/div[2]/div/form/div[4]/button''').click()      #clicking login button
-sleep(8)
-
-driver.find_element_by_xpath('''//*[@id="allProductsTabs__resale"]/div''').click()          #clicking resale button
-sleep(8)
-
-
-
-page_source=driver.page_source         #fetching the page to parse and get the edit of each product
-soup = BeautifulSoup(page_source, "html.parser")
-tgs=soup('div')
-for tag in tgs:
-    lnk=tag.get('data-link','jjj')
-    r.append(lnk)
-for i in r:
-    if(i !='jjj'):
-        nxturl='https://sorry.i-can't-show-the-url/seller/edit-products-resale?ids=' + i[-10::1]
-        sleep(8)
-        driver.get(nxturl)       #moving forward to each product
-        page_source=driver.page_source      # fetching the page to parse and get the variant quantity
-        soup = BeautifulSoup(page_source, "html.parser")
-        tags=soup('div')
-        txt=soup.get_text()
-        txt=txt.strip().split(':')
-        ret=txt[4]
-        ret=re.findall(".([0-9]+)",ret)[0]        #ret is the retail price
-        dis=txt[5]
-        dis=re.findall(".([0-9]+)",dis)[0]        #dis is the price after discount
-        SKUcode=txt[2]
-        SKUcode=re.findall("([A-Za-z0-9]+)",SKUcode)[0]
-        retail.append(ret)
-        discount.append(dis)
-        skucode.append(SKUcode)
-        for tag in tags:
-            lnk=tag.get('products-data','jjj')
-            rr.append(lnk)
-data={'code':skucode, 'retail':retail, 'discount':discount}
-df = pd.DataFrame(data)             #df is the table containing skucode and corresponding retail and discounted price
+usr = '*********'
+pwd = '************'
 
 df1=pd.read_csv(filename)
-val=int()
-val1=float()
-val2=float()
-for j in rr:
-    if(j != 'jjj'):
-        reqrdthngs=j
-        js=json.loads(reqrdthngs)
+# options.add_experimental_option("excludeSwitches", ["enable-logging"])     #if you have error related to the 'bluetooth adapter failed' which is caused due to version mismatch between the webdriver and browser
+driver = webdriver.Firefox(executable_path=r'''C:\Program Files (x86)\Mozilla Maintenance Service\geckodriver.exe''')
+driver.get('https://***.*******.***')
+print ("Website Opened")
+sleep(3)
+try:
+    cookies = pickle.load(open("cookies.pkl", "rb"))
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+except:
+    driver.find_element_by_name('emailOrMobile').send_keys(usr)        #entering id
+    print ("Id entered")
 
-        for variants in (js[0]["productVariants"]):
-            k=js[0]["product"]["productName"]
-            kk=re.findall("\([Code:]+\s([A-Za-z0-9]+)",k)[0]          #kk is the skucode, variants["productVariantDescription"] is the variant name, variants["productVariantQuantity"] is the  variant quantity
-            for i, rows in df1.iterrows():
-                if(rows[32]==kk and rows[40]==variants["productVariantDescription"]):
-                    val=variants["productVariantQuantity"]
-                    df1.at[i,'Stock']=val
-                if(rows[32]==kk):
-                    val1=df.loc[(df['code']==kk),'discount']
-                    val2=df.loc[(df['code']==kk),'retail']
-                    df1.at[i,'Sale price']=val1
-                    df1.at[i,'Regular price']=val2
+    driver.find_element_by_name('loginPassword').send_keys(pwd)          #entering password
+    print ("Password entered")
 
-df1.to_csv('updated_CSVready_to_import.csv')
+    driver.find_element_by_xpath('''/html/body/div/div/div[1]/div[1]/div/div[1]/div/form/div[4]/button''').click()      #clicking login button
+    sleep(8)
+driver.get(baseurl)
+sleep(3)
+pgchoice = input("Do you want to scrap from beginning?...(y/n)")
+if(pgchoice == 'n'):
+    pgnum = int(input("Enter the page no."))
+    for b in range(pgnum):
+        driver.find_element_by_xpath('''//*[@id="sellerAllProductsResale"]/div[8]/button[2]''').click()
+        sleep(3)
+        baseurl=driver.current_url
+else:
+    pass
+
+while(btn):
+    print("page number =", pgnum)
+    page_source=driver.page_source         #fetching the page to parse and get the edit of each product
+    soup = BeautifulSoup(page_source, "html.parser")
+    tgs=soup('div')
+    tgs1=soup('button')
+    r.clear()
+    btn.clear()
+    retail.clear()
+    discount.clear()
+    skucode.clear()
+    for tag in tgs:
+        lnk=tag.get('data-link','jjj')
+        r.append(lnk)
+    for tag in tgs1:
+        bttn=tag.get('disabled','jjj')
+        btn.append(bttn)
+    r = list(filter(lambda a: a != 'jjj', r))
+    for i in range(0, len(r), 5):
+        constcode='https://***.*******.***/****/edit-products-resale?ids='
+        driver.get(constcode + r[i][-10::1])
+        try:
+            driver.execute_script("window.open('{}', 'secondtab');".format(constcode + r[i+1][-10::1]))
+        except:
+            pass
+        try:
+            driver.execute_script("window.open('{}', 'thirdtab');".format(constcode + r[i+2][-10::1]))
+        except:
+            pass
+        try:
+            driver.execute_script("window.open('{}', 'fourthtab');".format(constcode + r[i+3][-10::1]))
+        except:
+            pass
+        try:
+            driver.execute_script("window.open('{}', 'fifthtab');".format(constcode + r[i+4][-10::1]))
+        except:
+            pass
+        for m in range(5):
+            sleep(3)
+            driver.switch_to.window(driver.window_handles[m])
+            sleep(2)
+            page_source=driver.page_source      # fetching the page to parse and get the variant quantity
+            soup = BeautifulSoup(page_source, "html.parser")
+            tags=soup('div')
+            txt=soup.get_text()
+            txt=txt.strip().split(':')
+            ret=txt[4]
+            try:
+                ret=re.findall(".([0-9]+)",ret)[0]        #ret is the retail price
+            except:
+                print("Trying Again...")
+                sleep(2)
+                page_source=driver.page_source      # fetching the page to parse and get the variant quantity
+                soup = BeautifulSoup(page_source, "html.parser")
+                tags=soup('div')
+                txt=soup.get_text()
+                txt=txt.strip().split(':')
+                ret=txt[4]
+                try:
+                    ret=re.findall(".([0-9]+)",ret)[0]
+                except:
+                    SKUcode=txt[2]
+                    SKUcode=re.findall("([A-Za-z0-9]+)",SKUcode)[0]
+                    print("There is no Retail Price for product with SKUCode {}".format(SKUcode))
+                    fskucode.append(SKUcode)
+                    print("Skipping this product...")
+                    continue
+            dis=txt[5]
+            dis=re.findall(".([0-9]+)",dis)[0]        #dis is the price after discount
+            SKUcode=txt[2]
+            SKUcode=re.findall("([A-Za-z0-9]+)",SKUcode)[0]
+            sk[SKUcode] = (ret, dis)
+            for tag in tags:
+                lnk=tag.get('products-data','jjj')
+                if(lnk != 'jjj'):
+                    js=json.loads(lnk)
+
+                    for variants in (js[0]["productVariants"]):
+                        k=js[0]["product"]["productName"]
+                        kk=re.findall("\([Code:]+\s([A-Za-z0-9]+)",k)[0]          #kk is the skucode, variants["productVariantDescription"] is the variant name, variants["productVariantQuantity"] is the  variant quantity
+                        print(variants["productVariantDescription"],'has quantity',variants["productVariantQuantity"], 'of product with SKUCode', kk)
+                        try:
+                            jstlttr=re.findall("([0-9A-Za-z]+)",variants["productVariantDescription"])[0]
+                        except:
+                            jstlttr=variants["productVariantDescription"]
+                        if(jstlttr=='2XL'):
+                            jstlttr='XXL'
+                            print(kk, "and", jstlttr)
+                        for i, rows in df1.iterrows():
+                            if(rows[32]==kk):
+                                (val2, val1) = sk[kk]
+                                print(val1, val2)
+                                df1.at[i,'Sale price']=val1
+                                df1.at[i,'Regular price']=val2
+                                print("second success")
+                            if(rows[32]==kk and rows[40]==jstlttr):
+                                val=variants["productVariantQuantity"]
+                                df1.at[i,'Stock']=val
+                                print("success")
+
+            print("page number =", pgnum)
+            print("Changing window...")
+            if(m == 4):
+                driver.switch_to.window(driver.window_handles[0])
+                break
+    if(btn[-1]==''):
+        break
+    else:
+        pgnum += 1
+        driver.switch_to.window(driver.window_handles[0])
+        driver.get(baseurl)
+        driver.find_element_by_xpath('''//*[@id="sellerAllProductsResale"]/div[8]/button[2]''').click()
+        sleep(5)
+        baseurl=driver.current_url
+
+df1.to_csv('updated_CSVready_to_import.csv', index = False, quoting = csv.QUOTE_NONNUMERIC)
+print("Failed to update the details of product with following SKUCodes...")
+print(fskucode)
 print ("Done")
 input('Press Enter to quit')
 driver.quit()
-print("Finished")
+print("Finished...", datetime.now() - starttime)
